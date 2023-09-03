ff158b2609fc3d28b8f67899ac2e0051262101f2
Denis <netfixk@yandex.ru>
2023-09-03 12:31:04 +0300

    counter is working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
 import './index.scss';
+import { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button className="minus" onClick={() => setCount(count - 1)}>- Минус</button>
+        <button className="plus" onClick={() => setCount(count + 1)}>Плюс +</button>
       </div>
     </div>
   );
